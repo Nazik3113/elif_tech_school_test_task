3eb80aa45e87de3e4feaff5258ee1f20c5f9e98b
Nazarii <nazarii.spikhalskyi@gmail.com>
2022-07-13 21:18:37 +0300

    update_Seeds_Connection

--- backend/mariadb/seeds/index.js
+++ backend/mariadb/seeds/index.js
@@ -1,60 +1,66 @@
 import mariadb from "mariadb";
 import fs from "fs";
 
+function runSeeds() {
+    const connection = mariadb.createConnection({
+        host: process.env.MARIADB_HOST,
+        user: process.env.MARIADB_USER,
+        password: process.env.MARIADB_PASSWORD,
+        database: process.env.MARIADB_DB_NAME,
+        connectionLimit: 5,
+        multipleStatements: true,
+    });
 
-const connection = mariadb.createConnection({
-    host: process.env.MARIADB_HOST,
-    user: process.env.MARIADB_USER,
-    password: process.env.MARIADB_PASSWORD,
-    database: process.env.MARIADB_DB_NAME,
-    connectionLimit: 5,
-    multipleStatements: true,
-});
+    connection.then(async (conn) => {
+        conn.beginTransaction();
+    
+        try {
+            const createShopsTableQuery = fs.readFileSync("./mariadb/seeds/queries/create_shops_table.sql", {
+                encoding: "utf-8",
+            });
+            await conn.query(createShopsTableQuery);  
+    
+            const fillShopsTableQuery = fs.readFileSync("./mariadb/seeds/queries/fill_shops_table.sql", {
+                encoding: "utf-8",
+            });
+    
+            await conn.query(fillShopsTableQuery);  
+    
+            const createShopsItemsTableQuery = fs.readFileSync("./mariadb/seeds/queries/create_shops_items_table.sql", {
+                encoding: "utf-8",
+            });
+    
+            await conn.query(createShopsItemsTableQuery);  
+    
+            const fillShopsItemsTableQuery = fs.readFileSync("./mariadb/seeds/queries/fill_shop_items_table.sql", {
+                encoding: "utf-8",
+            });
+    
+            await conn.query(fillShopsItemsTableQuery); 
+            
+            const createOrdersTableQuery = fs.readFileSync("./mariadb/seeds/queries/create_orders_table.sql", {
+                encoding: "utf-8",
+            });
+    
+            await conn.query(createOrdersTableQuery); 
+    
+            const createOrdersItemsTableQuery = fs.readFileSync("./mariadb/seeds/queries/create_orders_items_table.sql", {
+                encoding: "utf-8",
+            });
+    
+            await conn.query(createOrdersItemsTableQuery); 
+    
+            conn.commit();
+        } catch (error) {
+            conn.rollback();
+        }
+    
+        conn.end();
+    }).catch(err => {
+        setTimeout(() => {
+            runSeeds();
+        }, 2000);
+    });
+};
 
-
-connection.then(async (conn) => {
-    conn.beginTransaction();
-
-    try {
-        const createShopsTableQuery = fs.readFileSync("./mariadb/seeds/queries/create_shops_table.sql", {
-            encoding: "utf-8",
-        });
-        await conn.query(createShopsTableQuery);  
-
-        const fillShopsTableQuery = fs.readFileSync("./mariadb/seeds/queries/fill_shops_table.sql", {
-            encoding: "utf-8",
-        });
-
-        await conn.query(fillShopsTableQuery);  
-
-        const createShopsItemsTableQuery = fs.readFileSync("./mariadb/seeds/queries/create_shops_items_table.sql", {
-            encoding: "utf-8",
-        });
-
-        await conn.query(createShopsItemsTableQuery);  
-
-        const fillShopsItemsTableQuery = fs.readFileSync("./mariadb/seeds/queries/fill_shop_items_table.sql", {
-            encoding: "utf-8",
-        });
-
-        await conn.query(fillShopsItemsTableQuery); 
-        
-        const createOrdersTableQuery = fs.readFileSync("./mariadb/seeds/queries/create_orders_table.sql", {
-            encoding: "utf-8",
-        });
-
-        await conn.query(createOrdersTableQuery); 
-
-        const createOrdersItemsTableQuery = fs.readFileSync("./mariadb/seeds/queries/create_orders_items_table.sql", {
-            encoding: "utf-8",
-        });
-
-        await conn.query(createOrdersItemsTableQuery); 
-
-        conn.commit();
-    } catch (error) {
-        conn.rollback();
-    }
-
-    conn.end();
-});
+runSeeds();
