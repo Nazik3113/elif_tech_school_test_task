4982654c6fd4632661a7c49701a7e4e6ba7f605e
Nazarii <nazarii.spikhalskyi@gmail.com>
2022-07-14 15:47:48 +0300

    update_data_asseset

--- backend/mariadb/seeds/queries/fill_shops_table.sql
+++ backend/mariadb/seeds/queries/fill_shops_table.sql
@@ -1,7 +1,7 @@
 insert into `elif_tech_school_test_tack_db`.`shops`(`name`) values 
-                                            ('Rozetka'), 
-                                            ('Comfy'), 
-                                            ('Foxtrot'), 
-                                            ('Citrus'), 
-                                            ('Itbox')
+                                            ('Yama'), 
+                                            ('Бібліотека кафе'), 
+                                            ('Starwood'), 
+                                            ('Kebab story'), 
+                                            ('KFC')
 ;
